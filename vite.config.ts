f390ad766acc1180413c5675d3095115ef8abc21
Lango <whl2644992002@gmail.com>
2023-05-11 20:06:46 +0800

    juyi ip更改

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import {VantResolver} from 'unplugin-vue-components/resolvers';
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), Components({
         resolvers: [VantResolver()],
     }),],
+    server: {
+        port: 3000, // 端口号
+    }
 })
 
